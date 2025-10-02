f2ae44e7e9e57cfb7b1af79fe90524fc1727fe7f
Guliver Potsangbam <guliverpotsangbam@gmail.com>
2025-10-02 22:33:06 +0530

    test-change

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
     const location = useLocation();
 
     const navLinks = [
-        { href: "/", label: "Homo" },
+        { href: "/", label: "Home" },
         { href: "/about", label: "About" },
         { href: "/features", label: "Features" },
         { href: "/contact", label: "Contact" },
